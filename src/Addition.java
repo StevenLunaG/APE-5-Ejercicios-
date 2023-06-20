053101daf735842f411730c25561a68a83e235f7
StivenPabloJimenezCastillo <134020456+StivenPabloJimenezCastillo@users.noreply.github.com>
2023-06-19 22:19:50 -0500

    SumaDosNumeros

---
+++ src/Addition.java
@@ -0,0 +1,17 @@
+public class Addition {
+    //Atributos
+    private int num1;
+    private int num2;
+
+    //Constructor
+    public Addition(int num1, int num2) {
+        this.num1 = num1;
+        this.num2 = num2;
+    }
+
+    //Metodos
+    public int add(int num1, int num2) {
+        return num1 + num2;
+    }
+
+}
